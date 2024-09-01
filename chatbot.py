6186c78a9f8b6b858dc682628278df165b3cc156
Yu-Wei Liao <skyjadel@gmail.com>
2024-09-01 11:02:51 +0800

    Add files via upload
    Update code in main directory

---
+++ chatbot.py
@@ -0,0 +1,80 @@
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_openai import ChatOpenAI
+
+from openai import RateLimitError
+
+import pandas as pd
+from copy import deepcopy
+import numpy as np
+import datetime
+
+historical_data_path = './historical/data/'
+api_key_path = 'D:/資料科學與人工智慧實驗室/授權/openai/key.txt'
+model = 'gpt-4'
+
+def build_rag_df(historical_data_path=historical_data_path):
+    eval_df = pd.read_csv(historical_data_path + 'prediction/evaluation.csv')[0:-1]
+    eval_df['日期'] = pd.to_datetime(eval_df['日期'])
+    pred_df = pd.read_csv(historical_data_path + 'prediction/power.csv')
+    pred_df['日期'] = pd.to_datetime(pred_df['日期'])
+
+    for col in eval_df.columns:
+        if col != '日期':
+            eval_df[col] = [float(eval_df[col].iloc[i]) for i in range(len(eval_df))]
+
+    rag_df = deepcopy(eval_df)
+
+    this_dict = {k:[] for k in rag_df.columns}
+    for i in pred_df.index:
+        if not pred_df['日期'].loc[i] in list(rag_df['日期']):
+            for key in this_dict.keys():
+                if key == '日期':
+                    this_dict[key].append(pred_df['日期'].loc[i])
+                elif '_預測' in key:
+                    this_dict[key].append(pred_df[key.split('_')[0]].loc[i])
+                else:
+                    this_dict[key].append(np.nan)
+    rag_df = pd.concat([rag_df, pd.DataFrame(this_dict)], axis=0, ignore_index=True).reset_index(drop=True)
+
+    col_map = {}
+    for col in rag_df.columns:
+        if not col == '日期':
+            if '尖峰負載' in col:
+                col_map[col] = col.replace('尖峰負載', '尖峰負載(aka總發電量)') + '(MW)'
+            else:
+                col_map[col] = col + '(MW)'
+            rag_df[col] *= 10
+
+    rag_df.rename(col_map, axis=1, inplace=True)
+    return rag_df
+
+def create_agent(api_key_path=api_key_path, df=build_rag_df()):
+    with open(api_key_path, 'r') as f:
+        api_key = f.read()
+    
+    agent = create_pandas_dataframe_agent(
+        ChatOpenAI(
+            temperature=0.3,
+            model=model,
+            api_key=api_key
+        ),
+        df,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        allow_dangerous_code=True
+    )
+    return agent
+
+agent = create_agent()
+
+def respond_generator(input_message, agent=agent):
+    print('='*30)
+    print(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S'))
+    try:
+        respond = agent.invoke(input_message)
+        return respond['output']
+    except RateLimitError:
+        return "已超過請求限制，請稍後再試。"
+    except Exception as e:
+        return f'發生問題：{e}'
---
+++ dashboard.py
@@ -0,0 +1,279 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from copy import deepcopy
+import datetime
+import time
+
+from chatbot import respond_generator as chatbot_response
+
+#----------Initialization-----------
+
+realtime_data_path = './realtime/realtime_data/'
+historical_data_path = './historical/data/prediction/'
+y_feature_list = ['尖峰負載', '風力', '太陽能']
+
+now = datetime.datetime.now()
+
+first_hour_show_today_peak = 12
+
+#   Define power units
+units = {
+    '風力': 'MW',
+    '太陽能': 'MW',
+    '尖峰負載': 'GW'
+}
+
+#   The original values in the csv files are in the unit of ten-million-watts.
+#   We define the converting factors here. 
+unit_convert_factor = {
+    'KW': 10000,
+    'MW': 10,
+    'GW': 0.01
+}
+
+unit_factor = {}
+for k, u in units.items():
+    unit_factor[k] = unit_convert_factor[u]
+
+# Read predictions and observations
+eval_df = pd.read_csv(f'{historical_data_path}evaluation.csv')
+power_pred_df = pd.read_csv(f'{historical_data_path}power.csv')
+
+# Define metric layout
+if list(power_pred_df['日期']).index(eval_df['日期'].iloc[-2]) == len(power_pred_df) - 2:
+    second_row_first_col = 1
+elif list(power_pred_df['日期']).index(eval_df['日期'].iloc[-2]) == len(power_pred_df) - 3:
+    second_row_first_col = 2
+
+if now.hour >= first_hour_show_today_peak or ((now.hour == 0 and now.minute <= 40) and second_row_first_col==2):
+    peak_today_df = pd.read_csv(f'{realtime_data_path}peak.csv')
+    peak_today = peak_today_df.to_dict(orient='list')
+
+#--------Functions----------
+
+def value_string(val, unit):
+    if val >= 1000:
+        return f'{val:.0f} {unit}'
+    if val >= 100:
+        return f'{val:.1f} {unit}'
+    if val >= 10:
+        return f'{val:.2f} {unit}'
+    return f'{val:.3f} {unit}'
+
+def one_tab(y_feature, second_row_first_col):
+    this_unit = units[y_feature]
+    this_unit_factor = unit_factor[y_feature]
+
+    err = [np.abs(float(eval_df[f'{y_feature}_預測'].iloc[i]) - float(eval_df[y_feature].iloc[i])) * this_unit_factor for i in range(len(eval_df) - 1)]
+    avg_err = [float(eval_df[f'{y_feature}7日內平均誤差'].iloc[i]) * this_unit_factor for i in range(len(eval_df) - 1)]
+
+    left, right = st.columns(2)
+
+    left.header(y_feature)
+    left.markdown('#### 最新預測')
+
+    first_row = list(left.columns(4))
+    for j in range(4):
+        first_row[j].metric(
+            label=f"{power_pred_df['日期'].iloc[-j-1]} 預測",
+            value=value_string(power_pred_df[y_feature].iloc[-j-1] * this_unit_factor, this_unit), 
+        )
+
+    left.markdown('#### 近日預測表現')
+
+    second_row = list(left.columns(4))
+
+    if now.hour >= first_hour_show_today_peak:
+        today_str = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+        second_row[second_row_first_col-1].metric(
+            label=f"今日 ({today_str}) 推定數據",
+            value=value_string(peak_today[y_feature][0] * this_unit_factor, this_unit)
+        )
+    elif (now.hour == 0 and now.minute <= 40) and second_row_first_col==2:
+        yesterday_str = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(days=1), '%Y-%m-%d')
+        second_row[second_row_first_col-1].metric(
+            label=f"昨日 ({yesterday_str}) 推定數據",
+            value=value_string(peak_today[y_feature][0] * this_unit_factor, this_unit)
+        )
+
+
+    for i, j in enumerate(range(second_row_first_col, 4)):
+        second_row[j].metric(
+            label=f"{eval_df['日期'].iloc[-i-2]} 真實數據",
+            value=value_string(float(eval_df[y_feature].iloc[-i-2]) * this_unit_factor, this_unit),
+        )
+
+    third_row = list(left.columns(4))
+
+    if now.hour >= first_hour_show_today_peak:
+        this_err = np.abs(peak_today[y_feature][0] - power_pred_df[y_feature].iloc[-second_row_first_col]) * this_unit_factor
+        last_err = err[-1]
+        third_row[second_row_first_col-1].metric(
+            label=f"今日 ({today_str}) 推定誤差",
+            value=value_string(this_err, this_unit),
+            delta=value_string(this_err - last_err, this_unit),
+            delta_color='inverse',
+        )
+    elif (now.hour == 0 and now.minute <= 40) and second_row_first_col==2:
+        this_err = np.abs(peak_today[y_feature][0] - power_pred_df[y_feature].iloc[-second_row_first_col]) * this_unit_factor
+        last_err = err[-1]
+        third_row[second_row_first_col-1].metric(
+            label=f"昨日 ({yesterday_str}) 推定誤差",
+            value=value_string(this_err, this_unit),
+            delta=value_string(this_err - last_err, this_unit),
+            delta_color='inverse',
+        )
+
+    for i, j in enumerate(range(second_row_first_col, 4)):
+        this_err = err[-i-1]
+        last_err = err[-i-2]
+        third_row[j].metric(
+            label=f"{eval_df['日期'].iloc[-i-2]} 預測誤差",
+            value=value_string(this_err, this_unit),
+            delta=value_string(this_err - last_err, this_unit),
+            delta_color='inverse',
+        )
+
+    forth_row = list(left.columns(4))
+
+    if now.hour >= first_hour_show_today_peak or (now.hour == 0 and now.minute <= 40) and second_row_first_col==2:
+        today_str = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+        forth_row[second_row_first_col-1].metric(
+            label=f"今日尖峰時間",
+            value=peak_today['尖峰時間'][0]
+        )
+
+    for i, j in enumerate(range(second_row_first_col, 4)):
+        this_val = avg_err[-i-1]
+        last_val = avg_err[-i-2]
+        forth_row[j].metric(
+            label=f"{eval_df['日期'].iloc[-i-2]}, 7日內平均誤差",
+            value=value_string(this_val, this_unit),
+            delta=value_string(this_val - last_val, this_unit),
+            delta_color='inverse',
+        )
+
+    fifth_row = list(left.columns(4))
+    fifth_row[0].metric(
+        label='真實數據歷史標準差',
+        value=value_string(float(eval_df[f'{y_feature}7日內平均誤差'].iloc[-1]) * this_unit_factor, this_unit),
+    )
+
+    right.markdown('#### 歷史預測表現')
+
+    df = deepcopy(eval_df[0:-1])
+    if len(df) > 60:
+        df = deepcopy(df[-60::])
+    df['日期'] = pd.to_datetime(df['日期'])
+    df[f'{y_feature}'] = [float(v) * this_unit_factor for v in df[f'{y_feature}']]
+    df[f'{y_feature}_預測'] = [float(v) * this_unit_factor for v in df[f'{y_feature}_預測']]
+    df[f'{y_feature}7日內平均誤差'] = [float(v) * this_unit_factor for v in df[f'{y_feature}7日內平均誤差']]
+    df[f'{y_feature}誤差'] = np.abs(df[f'{y_feature}_預測'] - df[f'{y_feature}'])
+
+    # 使用 Plotly Express 創建長條圖
+    # bar_fig = px.bar(df, x='日期', y=f'{y_feature}誤差',
+    #                  labels={f'{y_feature}誤差': '每日誤差'})
+    line_fig = px.line(df, x='日期', y=f'{y_feature}7日內平均誤差', color_discrete_sequence=['gray'],
+                       labels={f'{y_feature}7日內平均誤差': '七日平均誤差'})
+
+    fig = go.Figure()
+
+    for trace in line_fig.data:
+        fig.add_trace(trace)
+
+    # for trace in bar_fig.data:
+    #     fig.add_trace(trace)
+
+    fig.update_layout(
+        title=f'七日平均誤差 ({this_unit})',
+        xaxis_title='日期',
+        yaxis_title=f'{y_feature}誤差',
+    )
+
+    fig.update_traces(selector=dict(type='scatter'), mode='lines+markers+text', textposition="top center")
+    right.plotly_chart(fig)
+
+    new_dict = {
+        '日期': [],
+        '類型': [],
+        '發電量': []
+    }
+
+    for i in range(len(df)):
+        new_dict['日期'].append(df['日期'].iloc[i])
+        new_dict['類型'].append('預測')
+        new_dict['發電量'].append(df[f'{y_feature}_預測'].iloc[i])
+
+        new_dict['日期'].append(df['日期'].iloc[i])
+        new_dict['類型'].append('真實數據')
+        new_dict['發電量'].append(df[f'{y_feature}'].iloc[i])
+
+    new_df = pd.DataFrame(new_dict)
+
+    fig = px.line(new_df, x='日期', y='發電量', color='類型', title=f'{y_feature}的預測與真實數據 ({this_unit})')
+    fig.update_traces(mode='lines+markers+text', textposition="top center")
+
+    right.plotly_chart(fig)
+
+
+    left.text('台灣時間每日 00:30 更新前一日真實發電量資料與預測表現, 每日 19:30 更新次日發電量預測')
+
+def AI_assistant():
+    def prompt_generator(input_message):
+        return f'背景知識：今天是{datetime.datetime.now().date()}, 對話內容：{input_message}'
+    # Streamed response emulator
+    def response_generator(input_message):
+        prompt = prompt_generator(input_message)
+        response = chatbot_response(prompt)
+        for word in response:
+            yield word
+            time.sleep(0.05)
+
+
+    st.header('聊天機器人')
+    st.markdown('#### 問題範例')
+    st.text('今天的風力發電預計佔總發電量多少比例？')
+    st.text('過去七天當中，太陽能發電預測的誤差平均多少？')
+    st.text('過去七天當中，哪一天的風力發電預測誤差最小，那天的預測值與實際值分別多少？')
+    st.text('整個八月中旬，台電的尖峰負載平均有多少？')
+    #st.text('如果發現機器人給的資訊好像過時了，請重新整理頁面，讓機器人回去資料庫看看最新數據。')
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for i, message in enumerate(st.session_state.messages):
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("可以問我關於台電太陽能與風力發電的預測與實際資料，以及尖峰負載的相關問題。"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            response = st.write_stream(response_generator(prompt))
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+#------------Main------------
+
+st.set_page_config(
+    page_title='台電綠能尖峰發電預測',  
+    page_icon=':high_brightness:',
+    layout='wide',
+)
+
+st.title('台電綠能尖峰發電預測')
+st.text('By Y. W. Liao')
+tabs = list(st.tabs(y_feature_list + ['聊天機器人']))
+
+for i, tab in enumerate(tabs[0:-1]):
+    with tab:
+        one_tab(y_feature_list[i], second_row_first_col)
+
+with tabs[-1]:
+    AI_assistant()
+    
