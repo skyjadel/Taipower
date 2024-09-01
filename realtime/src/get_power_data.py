b7fa9d66ebc88ce37cd75583f99816aa3449e038
Yu-Wei Liao <skyjadel@gmail.com>
2024-09-01 10:19:10 +0800

    Delete realtime/src directory

--- realtime/src/get_power_data.py
+++
@@ -1,9 +0,0 @@
-from crawler import power_generation
-
-sql_db_path = '/opt/airflow/sql_db/realtime.db'
-
-def main():
-    power_generation.get_data(sql_db_path)
-
-# if __name__ == '__main__':
-#     main()
