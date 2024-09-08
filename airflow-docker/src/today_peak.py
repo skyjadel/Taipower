69c4bfb7aa759a4bc35509a7ed626409351d72d1
Yu-Wei Liao <skyjadel@gmail.com>
2024-09-08 08:13:52 +0800

    [Debug] 修改 solar_energy_day_only 為 False

--- airflow-docker/src/today_peak.py
+++ airflow-docker/src/today_peak.py
@@ -7,7 +7,7 @@ def get_power_generation_at_peak(sql_db):
     if now.hour == 0 and now.minute <= 40:
         today -= datetime.timedelta(days=1)
 
-    df = get_oneday_power_data(sql_db, today, solar_energy_day_only=True)
+    df = get_oneday_power_data(sql_db, today, solar_energy_day_only=False)
     result = {
         '尖峰負載': df['尖峰負載'].iloc[0]/10,
         '風力': df['風力發電'].iloc[0],
