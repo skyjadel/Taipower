e3379a42b95a9e9bf3e616c66e2faa428923e7b1
Yu-Wei Liao <skyjadel@gmail.com>
2024-09-11 22:29:04 +0800

    [Mod] 針對儀表板供電結構圖的選擇時間功能，修改定期作業內容

--- airflow-docker/src/realtime_data_download.py
+++ airflow-docker/src/realtime_data_download.py
@@ -4,18 +4,19 @@ from crawler import power_generation
 from crawler import weather_forecast
 from crawler import weather_observation
 from today_peak import get_power_generation_at_peak
-from utils.tree_dataframe import save_tree_dfs
+from save_tree_dfs import save_tree_dfs
 
 realtime_data_path = '/opt/airflow/sql_db/'
+power_structure_path = '/opt/airflow/historical_data/power/power_structure/'
 
 sql_db_path = '/opt/airflow/sql_db/realtime.db'
 data_path = '/opt/airflow/sql_db/'
 test_db_path = './test.db'
 
-def main_power(sql_db_path=sql_db_path, data_path=data_path):
+def main_power(sql_db_path=sql_db_path, data_path=data_path, power_structure_path=power_structure_path):
     # 爬取電力資料，並存成 SQL 資料庫與 csv 檔
     power_generation.get_data(sql_db_path)
-    save_tree_dfs(sql_db_path, realtime_data_path)
+    save_tree_dfs(sql_db_path, power_structure_path)
     peak_dict = get_power_generation_at_peak(sql_db_path)
     pd.DataFrame(peak_dict, index=[0]).to_csv(f'{data_path}peak.csv', index=False, encoding='utf-8-sig')
     
