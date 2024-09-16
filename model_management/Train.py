498d714c63364957dd75ec5488b62c2b52450bdf
Yu-Wei Liao <skyjadel@gmail.com>
2024-09-17 05:07:13 +0800

    [Mod] 可以在每次訓練指定使用哪些氣象站

--- model_management/Train.py
+++ model_management/Train.py
@@ -4,6 +4,7 @@ import datetime
 
 from model_management.Ensemble_model import Ensemble_Model
 from Pytorch_models.metrics import Array_Metrics
+from utils.station_info import effective_station_list
 
 MAE = Array_Metrics.mae
 R2_score = Array_Metrics.r2
@@ -38,7 +39,8 @@ def save_model_metadata(file_path, model_xcols, model_hyperparameters_dict, opti
 
 
 # 為單一一種被預測值訓練模型
-def train_one_model(Y_feature, model_path, meta_path, data_path, start_date='2023-08-01', end_date='2200-12-31',
+def train_one_model(Y_feature, model_path, meta_path, data_path,
+                    start_date='2023-08-01', end_date='2200-12-31', effective_station_list=effective_station_list,
                     test_size=0.2, test_last_fold=True, apply_night_peak=False, remove_night_peak_samples=True,
                     latest_model_path=latest_model_path):
     with open(f'{meta_path}{Y_feature}/meta.json', 'r') as f:
@@ -59,12 +61,13 @@ def train_one_model(Y_feature, model_path, meta_path, data_path, start_date='202
         NP_X_feature_dict = NP_meta['X_feature_dict']
         NP_hyperparameters_dict = NP_meta['hyperparameters_dict']
         NP_weights = NP_meta['weights']
-
         MODEL = Ensemble_Model(Y_feature=Y_feature, 
                                X_feature_dict=X_feature_dict, 
                                hyperparameters_dict=hyperparameters_dict, 
                                data_path=data_path, 
                                weights=weights,
+                               effective_station_list=effective_station_list,
+                               start_date=start_date,
                                end_date=end_date,
                                test_size=test_size,
                                test_last_fold=test_last_fold,
@@ -79,6 +82,7 @@ def train_one_model(Y_feature, model_path, meta_path, data_path, start_date='202
                                hyperparameters_dict=hyperparameters_dict, 
                                data_path=data_path,
                                weights=weights, 
+                               effective_station_list=effective_station_list,
                                start_date=start_date, 
                                end_date=end_date, 
                                test_size=test_size,
@@ -95,18 +99,22 @@ def train_one_model(Y_feature, model_path, meta_path, data_path, start_date='202
 
 
 # 訓練一組能夠預測多個特徵的模型
-def train_all_models(model_path, meta_path, data_path, test_size=0.001, test_last_fold=False,
+def train_all_models(model_path, meta_path, data_path, effective_station_list=effective_station_list,
+                     test_size=0.001, test_last_fold=False,
                      apply_night_peak=False, remove_night_peak_samples=True, latest_model_path=latest_model_path,
                      start_date='2023-08-01', end_date='2200-12-31'):
     YF_list = os.listdir(meta_path)
     for Y_feature in YF_list:
         print(f'{Y_feature} 訓練中.')
-        train_one_model(Y_feature, model_path, meta_path, data_path, test_size=test_size, test_last_fold=test_last_fold,
+        train_one_model(Y_feature, model_path, meta_path, data_path, 
+                        effective_station_list=effective_station_list,
+                        test_size=test_size, test_last_fold=test_last_fold,
                         apply_night_peak=apply_night_peak, remove_night_peak_samples=remove_night_peak_samples, latest_model_path=latest_model_path,
                         start_date=start_date, end_date=end_date)
 
 
 def main_train(params=params_dict, preserved_days=0, train_model_main_path=train_model_main_path,
+               effective_station_list=effective_station_list,
                apply_night_peak=False, remove_night_peak_samples=True,
                latest_model_path=None):
     '''整合輸入參數，並訓練一組能夠預測多個特徵的模型
@@ -114,6 +122,7 @@ def main_train(params=params_dict, preserved_days=0, train_model_main_path=train
         params(dict, optional): 部份訓練參數，格式參看模組開頭的預設值
         preserved_days(int, optional): 保留幾天的最新資料不參與訓練，通常是為了事後驗證模型的表現
         train_model_main_path(str, optional): 模型儲存位置
+        effective_station_list(List of str, optional): 模型考慮的氣象站列表
         apply_night_peak(bool, optional): 太陽能部分是否加入夜尖峰調整
         remove_night_peak_samples(bool, optional): 太陽能訓練集是否排除夜尖峰樣本
         latest_model_path(str or None, optional): 若不是 None 則自動儲存一份模型參數到指定位置
@@ -121,6 +130,7 @@ def main_train(params=params_dict, preserved_days=0, train_model_main_path=train
     today = datetime.datetime.now().date()
     end_date = today - datetime.timedelta(days=preserved_days+1)
     end_date = datetime.datetime.strftime(end_date, '%Y-%m-%d')
+    params['effective_station_list'] = effective_station_list
     params['end_date'] = end_date
     params['apply_night_peak'] = apply_night_peak
     params['remove_night_peak_samples'] = remove_night_peak_samples
