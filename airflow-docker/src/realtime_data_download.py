e02c2b25e8d79e49f69a04206a7d1b5032ace17e
Yu-Wei Liao <skyjadel@gmail.com>
2024-10-02 01:27:25 +0800

    [Mod] 修改 peak.csv 的更新條件

--- airflow-docker/src/realtime_data_download.py
+++ airflow-docker/src/realtime_data_download.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 from crawler import power_generation
 from crawler import weather_forecast
@@ -23,9 +24,13 @@ def main_power(sql_db_path=sql_db_path,
                peak_data_path=peak_data_path):
     # 爬取電力資料，並存成 SQL 資料庫與 csv 檔
     power_generation.get_data(sql_db_path)
+    # 存樹狀圖需要的表格
     save_tree_dfs(sql_db_path, power_structure_path)
-    peak_dict = get_power_generation_at_peak(sql_db_path)
-    pd.DataFrame(peak_dict, index=[0]).to_csv(f'{realtime_data_path}peak.csv', index=False, encoding='utf-8-sig')
+    # 提取當日尖峰時刻相關資料
+    if datetime.datetime.now().hour >= 6:
+        peak_dict = get_power_generation_at_peak(sql_db_path)
+        pd.DataFrame(peak_dict, index=[0]).to_csv(f'{realtime_data_path}peak.csv', index=False, encoding='utf-8-sig')
+    # 更新給機器人查詢的 SQL Database
     save_RAG_SQL(sql_db_fn=rag_sql_db_fn, peak_data_path=peak_data_path, power_data_path=power_structure_path)
     
 def main_weather(sql_db_path=sql_db_path):
