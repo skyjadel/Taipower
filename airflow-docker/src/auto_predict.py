c162820d88e9d66dde8aeb2eafce5bb0f3265caf
Yu-Wei Liao <skyjadel@gmail.com>
2024-10-18 03:53:52 +0800

    [Mod] 把 wind_speed_naive 設為 False

--- airflow-docker/src/auto_predict.py
+++ airflow-docker/src/auto_predict.py
@@ -4,7 +4,10 @@ test_data_path = '../../historical/data/'
 test_model_path = '../../trained_model_parameters/latest_model/'
 
 def predict(data_path, model_path, predict_days=1):
-    _ = main_predict(data_path=data_path, model_path=model_path, predict_days=predict_days, wind_speed_naive=True)
+    _ = main_predict(data_path=data_path, 
+                     model_path=model_path,
+                     predict_days=predict_days,
+                     wind_speed_naive=False)
 
 if __name__ == '__main__':
     data_path = ''
